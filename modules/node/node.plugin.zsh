f223f670ea77e751863489e99fa68754e0362344
Andrew Thauer <andrewthauer@hotmail.com>
2015-09-06 10:22:13 -0400

    nvm auto switching & other minor updates

--- modules/node/node.plugin.zsh
+++ modules/node/node.plugin.zsh
@@ -1,8 +1,20 @@
+# setup nvm (node version manager)
 export NVM_DIR=~/.nvm
 source $(brew --prefix nvm)/nvm.sh
 
 if [ -z "${NVM_VER}" ]; then
-  echo ${fg[yellow]}"Warning - 'NVM_VER' not set"${reset_color}
+  echo ${fg[yellow]}"Warning - 'NVM_VER' not set -- using 'system'"${reset_color}
 else
   nvm use $NVM_VER > /dev/null
 fi
+
+# update nvm current node version automatically
+function chpwd() {
+  if [ -r $PWD/.nvmrc ]; then
+    nvm use `cat $PWD/.nvmrc`
+  fi
+}
+
+# aliases
+alias npm-outdated="npm outdated --depth 0 -q"
+alias npm-outdated-g="npm outdated -g --depth 0 -q"
