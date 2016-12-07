9ff4c507643169f95298396e75fd245f49bc1c13
Andrew Thauer <andrewthauer@hotmail.com>
2016-12-07 12:05:34 -0500

    adding additional aliases

--- bash/bash_aliases.sh
+++ bash/bash_aliases.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env sh
 
-# some more ls aliases
-alias l="ls -la"
+# directories
+alias cd..="cd .."
+alias ll="ls -lhA"
+alias lt="ls -ltA"
+alias lsl="ls -lhA | ${PAGER}"
+alias mkdir="mkdir -p"
+
+# find here
+alias fhere="find . -name"
+
+# process
+alias psg="ps aux | grep -v grep | grep -i"
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,12 @@
 #!/usr/bin/env sh
 
-alias zshconfig="${EDITOR} ~/.zshrc"
-alias reload!="zgen update && source ${HOME}/.zshrc"
+# common bash aliases
+source "${DOTFILES}/bash/bash_aliases.sh"
+
+# editor shortcuts
 alias e="edit"
+alias ezsh="${EDITOR} ~/.zshrc"
+
+# reloading
+alias src="source ~/.zshrc"
+alias reload!="zgen update && source ${HOME}/.zshrc"
