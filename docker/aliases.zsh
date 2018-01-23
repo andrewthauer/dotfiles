c5ba453ac0ae358cf2c5d43db0a6d407f0c5b6bd
Andrew Thauer <andrewthauer@users.noreply.github.com>
2018-01-22 23:46:12 -0500

    minor updates

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -2,7 +2,7 @@
 # Docker related aliases
 #
 
-# Skip if docker isn't installed
+# Skip if docker is not installed
 if (( ! $+commands[docker] )); then
   return
 fi
@@ -18,16 +18,16 @@ alias di="docker images"
 alias dbu="docker build"
 alias drmi="docker rmi"
 alias drmi_all="docker rmi $* $(docker images -a -q)"
-alias drmi_dang='docker rmi $* $(docker images -q -f "dangling=true")'
+alias drmi_dang="docker rmi $* $(docker images -q -f dangling=true)"
 
 # Containers
 alias dps="docker ps"
 alias dpsa="docker ps -a"
-alias dpss='docker ps -a --format "table {{.ID}}\t{{.Names}}\t{{.Image}}\t{{.RunningFor}}\t{{.Status}}"'
+alias dpss="docker ps -a --format='table {{.ID}}\t{{.Names}}\t{{.Image}}\t{{.RunningFor}}\t{{.Status}}'"
 alias dpsl="docker ps -l $*"
 alias dstart="docker start $*"
 alias dstop="docker stop $*"
-alias dstop_all='docker stop $* $(docker ps -q -f "status=running")'
+alias dstop_all="docker stop $* $(docker ps -q -f status=running)"
 alias drestart="docker restart $*"
 alias dattach="docker attach $*"
 alias dexec="docker exec"
@@ -36,18 +36,18 @@ alias drunr="docker run --rm $*"
 alias drunit="docker run --rm -it $*"
 alias dinspect="docker inspect $*"
 alias dlog="docker logs"
-alias dip='docker inspect --format "{{ .NetworkSettings.IPAddress }}" $*'
+alias dip="docker inspect --format='{{.NetworkSettings.IPAddress}}' $*"
 alias drm="docker rm"
-alias drm_stopped='docker rm $* $(docker ps -q -f "status=exited")'
+alias drm_stopped="docker rm $* $(docker ps -q -f status=exited)"
 alias drm_all="docker rm $* $(docker ps -a -q)"
-alias drmv_stopped='docker rm -v $* $(docker ps -q -f "status=exited")'
+alias drmv_stopped="docker rm -v $* $(docker ps -q -f status=exited)"
 alias drmv_all="docker rm -v $* $(docker ps -a -q)"
 
 # Volumes
 alias dvls="docker volume ls $*"
 alias dvrm="docker volume rm $*"
 alias dvrm_all="docker volume rm $(docker volume ls -q)"
-alias dvrm_dang='docker volume rm $(docker volume ls -q -f "dangling=true")'
+alias dvrm_dang="docker volume rm $(docker volume ls -q -f dangling=true)"
 
 #
 # docker-compose
@@ -70,14 +70,4 @@ alias dcupd="docker-compose up -d"
 alias dcdn="docker-compose down"
 alias dcl="docker-compose logs"
 alias dclf="docker-compose logs -f"
-
-# Special helpers
 alias dctail="docker-compose logs --tail=all -f"
-
-#
-# docker-sync (docker-sync.io)
-#
-
-alias dsync="docker-sync"
-alias dsyncs="docker-sync start"
-alias dsyncsst="docker-sync-stack"
--- node/aliases.zsh
+++ node/aliases.zsh
@@ -9,3 +9,9 @@ alias npm-list-g="npm list -g --depth=0 2>/dev/null"
 # npm outdated
 alias npm-outdated="npm outdated --depth 0 -q"
 alias npm-outdated-g="npm outdated -g --depth 0 -q"
+
+# npm script shortcuts
+alias npms="npm start"
+alias npmt="npm test"
+alias npmb="npm run build"
+alias npml="npm run lint"
