058739e6c9f74807ac8e28f9c4111c6e2f0f5f0a
Andrew Thauer <6507159+andrewthauer@users.noreply.github.com>
2019-04-17 13:52:28 -0400

    fix nodenv

--- shell/init.sh
+++ shell/init.sh
@@ -3,17 +3,17 @@
 # programming environments
 #
 
-if [[ -s "${HOME}/.nodenv/bin/nodenv" ]]; then
+if [[ -x "$(command -v nodenv)" || -s "${HOME}/.nodenv/bin/nodenv" ]]; then
   export PATH="${HOME}/.nodenv/bin:${PATH}"
   eval "$(nodenv init - --no-rehash)"
 fi
 
-if [[ -s "${HOME}/.nodenv/bin/pyenv" ]]; then
+if [[ -x "$(command -v pyenv)" || -s "${HOME}/.pyenv/bin/pyenv" ]]; then
   export PATH="${HOME}/.pyenv/bin:${PATH}"
   eval "$(pyenv init - --no-rehash)"
 fi
 
-if [[ -s "${HOME}/.nodenv/bin/rbenv" ]]; then
+if [[ -x "$(command -v rbenv)" || -s "${HOME}/.rbenv/bin/rbenv" ]]; then
   export PATH="${HOME}/.rbenv/bin:${PATH}"
   eval "$(rbenv init - --no-rehash)"
 fi
