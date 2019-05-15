a8536701ac930a71e13bd5a474a6e30383406330
Andrew Thauer <6507159+andrewthauer@users.noreply.github.com>
2019-05-14 21:38:59 -0400

    add dotnet

--- modules/dotnet/init.sh
+++ modules/dotnet/init.sh
@@ -4,7 +4,18 @@
 
 # Initialize dotnet
 if [[ -x "$(command -v dotnet)" ]]; then
-  export DOTNET_ROOT=/usr/local/share/dotnet
-  export MSBuildSDKsPath=$DOTNET_ROOT/sdk/$(${DOTNET_ROOT}/dotnet --version)/Sdks
-  export PATH=${PATH}:${DOTNET_ROOT}
+  export DOTNET_ROOT="/usr/local/share/dotnet"
+  export PATH="${PATH}:${DOTNET_ROOT}"
+
+  # Will only be executed on the first call to 'dotnet'
+  dotnet() {
+    # Remove this function, subsequent calls will execute directly
+    unfunction "$0"
+
+    # Expensive operation
+    export MSBuildSDKsPath="${DOTNET_ROOT}/sdk/$(${DOTNET_ROOT}/dotnet --version)/Sdks"
+
+    # Execute binary
+    $0 "$@"
+  }
 fi
--- modules/zsh/.zshrc
+++ modules/zsh/.zshrc
@@ -14,13 +14,12 @@ source "${DOTFILES_MOD_DIR}/zsh/completion.zsh"
 
 # Load plugins
 source "${DOTFILES_MOD_DIR}/zsh/plugins.zsh"
-# source "${DOTFILES_MOD_DIR}/zsh/zplug-load.zsh"
 
 # Load modules
 source "${DOTFILES_MOD_DIR}/brew/aliases.sh"
 source "${DOTFILES_MOD_DIR}/docker/aliases.sh"
 source "${DOTFILES_MOD_DIR}/git/aliases.sh"
-# source "${DOTFILES_MOD_DIR}/dotnet/init.sh"
+source "${DOTFILES_MOD_DIR}/dotnet/init.sh"
 source "${DOTFILES_MOD_DIR}/dotfiles/init.zsh"
 source "${DOTFILES_MOD_DIR}/node/init.sh"
 source "${DOTFILES_MOD_DIR}/python/init.sh"
