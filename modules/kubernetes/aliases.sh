d99fdb493490e958b0a367623252223cb1bec84f
Andrew Thauer <6507159+andrewthauer@users.noreply.github.com>
2019-08-21 17:07:48 -0400

    update kube aliases

--- modules/kubernetes/aliases.sh
+++ modules/kubernetes/aliases.sh
@@ -6,8 +6,13 @@
 alias k="kubectl"
 alias kcheat="open https://kubernetes.io/docs/reference/kubectl/cheatsheet"
 
-# viewing resources
+# config & contexts
 alias kgc="kubectl config get-contexts"
+
+# resources
 alias kpods="kubectl get pods"
 alias kpodsa="kubectl get pods --all-namespaces"
 alias kpodsi="kpods -o jsonpath="{..image}" | tr -s '[[:space:]]' '\n' | sort | uniq -c"
+
+# logs
+alias klog="kubectl logs"
