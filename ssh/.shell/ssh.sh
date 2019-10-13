1939812c743dce5a96851210223eedaf21389f8b
Andrew Thauer <6507159+andrewthauer@users.noreply.github.com>
2019-10-13 11:14:01 -0400

    add ssh pkg

---
+++ ssh/.shell/ssh.sh
@@ -0,0 +1,11 @@
+#
+# SSH aliases
+#
+
+# local port forwarding
+#   ssh -L <local_port>:<remote_host>:<remote_port> user@<remote_server>
+alias sshl="ssh -L"
+
+# remote port forward
+#   ssh -R <remote_port>:<local_host>:<local_port> user@<remote_server>
+alias sshr="ssh -R"
