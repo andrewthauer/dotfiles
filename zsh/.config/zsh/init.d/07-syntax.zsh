eb16d3e684efe88a1e88cad751b6d742866650a6
Andrew Thauer <6507159+andrewthauer@users.noreply.github.com>
2019-10-20 23:05:50 -0400

    update zsh for linux

--- zsh/.config/zsh/init.d/07-syntax.zsh
+++ zsh/.config/zsh/init.d/07-syntax.zsh
@@ -2,29 +2,34 @@
 # Post completion script
 #
 
-# Exit if prefix is not set
-if [[ -z ${BREW_PREFIX} ]]; then
-  return 1
+# Set the share directory to source from
+if [[ -n ${BREW_PREFIX} ]]; then
+  SHARE_DIR="${BREW_PREFIX}/share"
+else
+  SHARE_DIR="${XDG_DATA_HOME}"
 fi
 
 # Load zsh prompt syntax highlighting
 # - https://github.com/zsh-users/zsh-syntax-highlighting
 # - NOTE: Must be loaded before zsh-history-substring-search
-if [[ -d "${BREW_PREFIX}/share/zsh-syntax-highlighting" ]]; then
+if [[ -d "${SHARE_DIR}/zsh-syntax-highlighting" ]]; then
   ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
-  source "${BREW_PREFIX}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+  source "${SHARE_DIR}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 fi
 
 # Load zsh history substring search
 # - https://github.com/zsh-users/zsh-history-substring-search
 # - NOTE: Must be loaded after zsh-syntax-highlighting
-if [[ -d "${BREW_PREFIX}/share/zsh-history-substring-search" ]]; then
-  source "${BREW_PREFIX}/share/zsh-history-substring-search/zsh-history-substring-search.zsh"
+if [[ -d "${SHARE_DIR}/zsh-history-substring-search" ]]; then
+  source "${SHARE_DIR}/zsh-history-substring-search/zsh-history-substring-search.zsh"
 fi
 
 # Load zsh auto suggestions
 # - https://github.com/zsh-users/zsh-autosuggestions
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#555555,bold,underline"
-if [[ -d "${BREW_PREFIX}/share/zsh-autosuggestions" ]]; then
-  source "${BREW_PREFIX}/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+if [[ -d "${SHARE_DIR}/zsh-autosuggestions" ]]; then
+  source "${SHARE_DIR}/zsh-autosuggestions/zsh-autosuggestions.zsh"
 fi
+
+# Cleanup
+unset SHARE_DIR
