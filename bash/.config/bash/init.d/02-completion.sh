675d59fdfd32352b0c552e9639bc79b81a665961
Andrew Thauer <6507159+andrewthauer@users.noreply.github.com>
2019-10-22 12:08:20 -0400

    speed up bash

--- bash/.config/bash/init.d/02-completion.sh
+++ bash/.config/bash/init.d/02-completion.sh
@@ -10,3 +10,10 @@ elif [[ -n ${BREW_PREFIX} && -f "${BREW_PREFIX}/etc/bash_completion" ]]; then
 elif [[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]]; then
   source "/usr/local/etc/profile.d/bash_completion.sh"
 fi
+
+# Load custom completions
+if [[ -d "${XDG_DATA_HOME}/bash_completion.d" ]]; then
+  for file in ${XDG_DATA_HOME}/bash_completion.d/*; do
+    source $file
+  done
+fi
