b17193ae0ea6fe88d882b60b9efbef35ec5364fe
Andrew Thauer <6507159+andrewthauer@users.noreply.github.com>
2019-11-26 21:46:23 -0500

    fix hammerspoon watcher

--- @macos/.config/hammerspoon/watcher.lua
+++ @macos/.config/hammerspoon/watcher.lua
@@ -6,4 +6,4 @@ function reload_config(files)
   hs.reload()
 end
 
-hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reload_config):start()
+hs.pathwatcher.new(os.getenv("HOME") .. "/.config/hammerspoon/", reload_config):start()
