ad0f19f5f02ec3b4b872eef4ef63db57b79372b7
Andrew Thauer <6507159+andrewthauer@users.noreply.github.com>
2020-11-11 23:32:36 -0500

    update kafka

--- opt/kafka/.config/shell.d/kafka.sh
+++ opt/kafka/.config/shell.d/kafka.sh
@@ -1,3 +1,11 @@
 #
 # Initialize kafka environment
 #
+
+# cli aliases
+alias kaf="kafka"
+
+# kafkacat
+alias kcat="kafkacat"
+alias kcat-local="kafkacat -b localhost:9092"
+alias kcat-local-r="kafkacat -b localhost:9092 -s avro -r http://localhost:8081"
--- opt/kafka/install.sh
+++ opt/kafka/install.sh
@@ -4,18 +4,14 @@
 #
 
 install_confluent_cli() {
-  pushd "${XDG_DATA_HOME}/bin" >/dev/null || return
-  curl -sL https://cnfl.io/cli | sh -s -- latest -b .
-  popd >/dev/null || return
+  curl -sL https://cnfl.io/cli | sh -s -- latest -b "${XDG_BIN_HOME}"
 
   confluent completion bash >|"${XDG_DATA_HOME}/bash_completion.d/confluent"
   confluent completion zsh >|"${XDG_DATA_HOME}/zsh/functions/_confluent"
 }
 
 install_confluent_cloud_cli() {
-  pushd "${XDG_DATA_HOME}/bin" >/dev/null || return
-  curl -L --http1.1 https://cnfl.io/ccloud-cli | sh -s -- -b .
-  popd >/dev/null || return
+  curl -L --http1.1 https://cnfl.io/ccloud-cli | sh -s -- -b "${XDG_BIN_HOME}"
 
   ccloud completion bash >|"${XDG_DATA_HOME}/bash_completion.d/ccloud"
   ccloud completion zsh >|"${XDG_DATA_HOME}/zsh/functions/_ccloud"
