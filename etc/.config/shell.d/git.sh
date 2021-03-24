a86a15ef966ae124ee12ee2c4774b413bb84e231
Andrew Thauer <6507159+andrewthauer@users.noreply.github.com>
2021-03-24 09:32:15 -0400

    git updates

--- etc/.config/shell.d/git.sh
+++ etc/.config/shell.d/git.sh
@@ -19,7 +19,7 @@ alias gbd='git branch -d'
 alias gci='git commit'
 alias gcb='git checkout -b'
 alias gcf='git config --list'
-alias gco='git checkout'
+# alias gco='git checkout'
 alias gcd='git checkout -b'
 alias gcm='git cm'
 alias gd='git diff'
@@ -28,3 +28,20 @@ alias gsc='git switch -c'
 alias gsd='git sd'
 alias gst='git status -sb'
 alias gwt='git worktree'
+
+#
+# Fuzzy
+#
+
+alias gfb="git fuzzy branch"
+alias gfd="git fuzzy diff"
+alias gfl="git fuzzy log"
+alias gfr="git fuzzy remote"
+alias gfst="git fuzzy status"
+alias gfsl="git fuzzy stash-list"
+alias gft="git fuzzy tag"
+
+gco() {
+  # shellcheck disable=SC2046
+  git checkout $(git fuzzy branch)
+}
