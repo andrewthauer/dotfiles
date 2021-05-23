9946de5528eb6737b124146fcd52428c5f522227
Andrew Thauer <6507159+andrewthauer@users.noreply.github.com>
2021-05-23 07:26:06 -0400

    disable zsh-user plugins (perf)

--- etc/.config/zsh/init.d/07-syntax.zsh
+++ etc/.config/zsh/init.d/07-syntax.zsh
@@ -3,33 +3,32 @@
 #
 
 # Set the share directory to source from
+SHARE_DIR="${XDG_DATA_HOME}"
 if [[ -n ${BREW_PREFIX} ]]; then
   SHARE_DIR="${BREW_PREFIX}/share"
-else
-  SHARE_DIR="${XDG_DATA_HOME}"
 fi
 
 # Load zsh prompt syntax highlighting
 # - https://github.com/zsh-users/zsh-syntax-highlighting
 # - NOTE: Must be loaded before zsh-history-substring-search
-if [[ -d "${SHARE_DIR}/zsh-syntax-highlighting" ]]; then
-  ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
-  source "${SHARE_DIR}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
-fi
+# if [[ -d "${SHARE_DIR}/zsh-syntax-highlighting" ]]; then
+#   ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
+#   source "${SHARE_DIR}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+# fi
 
 # Load zsh history substring search
 # - https://github.com/zsh-users/zsh-history-substring-search
 # - NOTE: Must be loaded after zsh-syntax-highlighting
-if [[ -d "${SHARE_DIR}/zsh-history-substring-search" ]]; then
-  source "${SHARE_DIR}/zsh-history-substring-search/zsh-history-substring-search.zsh"
-fi
+# if [[ -d "${SHARE_DIR}/zsh-history-substring-search" ]]; then
+#   source "${SHARE_DIR}/zsh-history-substring-search/zsh-history-substring-search.zsh"
+# fi
 
 # Load zsh auto suggestions
 # - https://github.com/zsh-users/zsh-autosuggestions
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#555555,bold,underline"
-if [[ -d "${SHARE_DIR}/zsh-autosuggestions" ]]; then
-  source "${SHARE_DIR}/zsh-autosuggestions/zsh-autosuggestions.zsh"
-fi
+# ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#555555,bold,underline"
+# if [[ -d "${SHARE_DIR}/zsh-autosuggestions" ]]; then
+#   source "${SHARE_DIR}/zsh-autosuggestions/zsh-autosuggestions.zsh"
+# fi
 
 # Cleanup
 unset SHARE_DIR
