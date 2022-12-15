b69f89bb9a230806b6ff48dff3a5018cee3ac874
Andrew Thauer <6507159+andrewthauer@users.noreply.github.com>
2022-12-14 21:28:58 -0500

    split out nvim

--- modules/vim/.config/profile.d/vim.sh
+++ modules/vim/.config/profile.d/vim.sh
@@ -3,4 +3,8 @@
 #
 
 # Remap ~/.vimrc for XDG specification
-export VIMINIT=":source $XDG_CONFIG_HOME/nvim/init.vim"
+if [ -f "$XDG_CONFIG_HOME/nvim/init.lua" ]; then
+  export VIMINIT=":source $XDG_CONFIG_HOME/nvim/init.lua"
+elif [ -f "$XDG_CONFIG_HOME/nvim/init.vim" ]; then
+  export VIMINIT=":source $XDG_CONFIG_HOME/nvim/init.vim"
+fi
