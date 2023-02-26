a643229944b3d68dc5eab9c2495479fc73d4b016
Andrew Thauer <6507159+andrewthauer@users.noreply.github.com>
2023-02-26 10:37:37 -0500

    switch to zoxide

---
+++ modules/zoxide/.config/shell.d/zoxide.sh
@@ -0,0 +1,31 @@
+#
+# Initialize zoxide
+#
+# - https://github.com/ajeetdsouza/zoxide
+#
+
+# shellcheck disable=SC1090
+
+# Ensure zoxide is installed
+if ! command_exists "zoxide"; then
+  return 1
+fi
+
+# The data location
+export _ZO_DATA_DIR="${XDG_DATA_HOME}/zoxide"
+
+# When set to 1, z will print the matched directory before navigating to it.
+export _ZO_ECHO=1
+
+# Initialize fasd for bash
+if [ -n "${BASH_VERSION}" ]; then
+  eval "$(zoxide init bash)"
+
+# Initialize fasd for zsh
+elif [ -n "${ZSH_VERSION}" ]; then
+  eval "$(zoxide init zsh)"
+
+# Fallback to posix initialize
+else
+  eval "$(zoxide init posix --hook prompt)"
+fi
---
+++ modules/zoxide/README.md
@@ -0,0 +1,7 @@
+# zoxide
+
+zoxide is a smarter cd command, inspired by z and autojump.
+
+## Resources
+
+- https://github.com/ajeetdsouza/zoxide
