135fa9b2e9e5bf18f1a268aab436eb09aad4ccb9
Andrew Thauer <6507159+andrewthauer@users.noreply.github.com>
2023-02-26 10:41:37 -0500

    update k8s

--- modules/kubernetes/.config/shell.d/kubernetes.sh
+++ modules/kubernetes/.config/shell.d/kubernetes.sh
@@ -2,9 +2,27 @@
 # Initialize kubernetes environment
 #
 
+#
+# helm
+#
+
 if command_exists "helm"; then
   # make helm more xdg compliant
   export HELM_HOME="${XDG_DATA_HOME}/helm"
+fi
+
+#
+# minikube
+#
+
+if command_exists "minikube"; then
+  # make minikube more xdg compliant
+  export MINIKUBE_HOME="${XDG_CONFIG_HOME}/minikube"
+
+  # aliases
+  alias mk="minikube"
+  alias mkk="minikube kubectl --"
+  alias minikube-inject-env='eval "$(minikube docker-env)"'
 fi
 
 #
@@ -18,7 +36,6 @@ if command_exists "kubectl"; then
   # make helm more xdg compliant
   export KUBECONFIG="${XDG_CONFIG_HOME}/kube/config"
   export KUBECACHEDIR="${XDG_CACHE_HOME}/kube"
-  export MINIKUBE_HOME="${XDG_CACHE_HOME}/minikube"
 
 # Return if requirements not found
 else
@@ -26,20 +43,57 @@ else
 fi
 
 #
-# Aliases
+# kubectl
 #
 
-# kubectl
+# general
 alias k='kubectl'
-alias kd='kubectl describe -'
-alias kga='kubectl get all -A'
-alias kgy='kubectl get -o yaml'
 alias khelp='open https://kubernetes.io/docs/reference/kubectl/cheatsheet'
-alias klog='kubectl logs'
-alias krestart='kubectl rollout restart'
-alias krestartd='kubectl rollout restart deployment'
-alias kexec='kubectl exec --stdin --tty'
 
-# minikube
-alias mk="minikube"
-alias mkk="minikube kubectl --"
+# describe
+alias kd='kubectl describe'
+
+# get
+alias kg='kubectl get'
+alias kga='kubectl get all -A'  # list all namespaces
+alias kgy='kubectl get -o yaml'
+
+# get additional
+alias kgconf='kubectl get configmap'
+alias kgsec='kubectl get secret'
+
+# logs
+alias klog='kubectl logs'
+
+# pods
+alias k-pod-names='kubectl get pods --no-headers -o custom-columns=":metadata.name"'
+
+# debugging
+alias keit='kubectl exec -it'
+alias ketty='kubectl exec --stdin --tty'
+
+# set default namespace
+k-set-ns() {
+  kubectl config set-context --current --namespace="$1"
+}
+
+# gets all resouce kinds
+k-get-kinds() {
+  for i in $(kubectl api-resources --verbs=list --namespaced -o name | grep -v "events.events.k8s.io" | grep -v "events" | sort | uniq); do
+    echo "$i"
+  done
+}
+
+# get all resources in a namespace
+k-get-all() {
+  local ns="$1"
+
+  for i in $(k-get-kinds); do
+    data=$(kubectl -n "${ns}" get --ignore-not-found "${i}")
+    if [ -n "$data" ]; then
+      echo "--- resource: $i ---"
+      echo "$data"
+      echo ""
+    fi
+  done
+}
