c0571ea482fbfcef730e0f62d855f751aca80b0b
Andrew Thauer <6507159+andrewthauer@users.noreply.github.com>
2023-04-09 09:38:16 -0400

    update rtx for devenv shell

--- modules/rtx/.config/shell.d/rtx.sh
+++ modules/rtx/.config/shell.d/rtx.sh
@@ -10,10 +10,23 @@ fi
 
 # XDG Specification
 export RTX_CACHE_DIR="${RTX_CACHE_DIR:-$XDG_CACHE_HOME}/rtx"
+
+# Use rtx toml instead of .tool-versions
 export RTX_USE_TOML=1
 
+#
+# Initialization
+#
+
+# Avoid issues with nix shells from devenv
+if command_exists "devenv"; then
+  if [ -n "$DEVENV_DIR" ]; then
+    export RTX_AUTO_ACTIVATE_DISABLED=1
+  fi
+fi
+
 # Initialize rtx
-if [ -z "$RTX_ACTIVATE_DISABLED" ]; then
+if [ -z "$RTX_AUTO_ACTIVATE_DISABLED" ]; then
   eval "$(rtx activate "$CURRENT_SHELL")"
 fi
 
@@ -21,12 +34,12 @@ fi
 # Lazy loaded rtx
 #
 
-rtxd() {
-  export RTX_ACTIVATE_DISABLED=1
-  rtx deactivate
+rtxa() {
+  unset RTX_AUTO_ACTIVATE_DISABLED
+  rtx activate
 }
 
-rtxa() {
-  unset RTX_ACTIVATE_DISABLED
-  rtx activate
+rtxd() {
+  export RTX_AUTO_ACTIVATE_DISABLED=1
+  rtx deactivate
 }
