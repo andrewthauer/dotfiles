7918210ecccbc0000a7876cae5c5fc5a1ba21c3f
Andrew Thauer <6507159+andrewthauer@users.noreply.github.com>
2023-04-10 23:24:46 -0400

    nix fixes

--- modules/nix/.config/profile.d/nix.sh
+++ modules/nix/.config/profile.d/nix.sh
@@ -10,8 +10,8 @@ fi
 # End Nix
 
 # For home-manager
-if [ -z "$NIX_PATH" ]; then
-  # export NIX_PATH="nixpkgs=/nix/var/nix/profiles/per-user/$USER/channels/nixpkgs:/nix/var/nix/profiles/per-user/$USER/channels"
-  # shellcheck disable=SC1091
-  export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
-fi
+# if [ -z "$NIX_PATH" ]; then
+#   # export NIX_PATH="nixpkgs=/nix/var/nix/profiles/per-user/$USER/channels/nixpkgs:/nix/var/nix/profiles/per-user/$USER/channels"
+#   # shellcheck disable=SC1091
+#   export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
+# fi
