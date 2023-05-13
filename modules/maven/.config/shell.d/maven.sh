92dfe0450165081b8c097b9d03ddbc8d4905e8a1
Andrew Thauer <6507159+andrewthauer@users.noreply.github.com>
2023-05-13 00:47:36 -0400

    disable maven home env

--- modules/maven/.config/shell.d/maven.sh
+++ modules/maven/.config/shell.d/maven.sh
@@ -12,7 +12,7 @@ fi
 #
 
 # Set the maven home
-export M2_HOME="${XDG_DATA_HOME}/sdk/maven/current"
+# export M2_HOME="${XDG_DATA_HOME}/sdk/maven/current"
 
 # Override the config/cache location
 alias mvn='mvn -gs "$XDG_CONFIG_HOME/maven/settings.xml"'
