6ba78017e590e209eadbc71826fa22af89cd90b9
Andrew Thauer <6507159+andrewthauer@users.noreply.github.com>
2024-01-24 23:20:36 -0500

    revise op-dotenv

--- bin/op-dotenv.ts
+++ bin/op-dotenv.ts
@@ -14,13 +14,14 @@
     }
 */
 
-import * as dotenv from "https://deno.land/std@0.211.0/dotenv/mod.ts";
-import { substitute } from "https://deno.land/x/substitute@v0.2.1/mod.ts";
+import * as dotenv from 'https://deno.land/std@0.212.0/dotenv/mod.ts';
+import { default as $ } from 'https://deno.land/x/dax@0.36.0/mod.ts';
+import { substitute } from 'https://deno.land/x/substitute@v0.2.1/mod.ts';
 
 // parse the args (note: currently assumes path is first arg)
 const envPath = Deno.args[0];
 if (!envPath) {
-  console.error("op-dotenv.ts <dotenv_path> [--export]")
+  console.error('op-dotenv.ts <dotenv_path> [--export]');
   Deno.exit(1);
 }
 
@@ -29,7 +30,11 @@ const contents = Deno.readTextFileSync(envPath);
 let env = dotenv.parse(contents);
 
 // resolve environment variables from 1password cli
-const command = new Deno.Command("op", { args: ["inject"], stdin: 'piped', stdout: "piped" });
+const command = new Deno.Command('op', {
+  args: ['inject'],
+  stdin: 'piped',
+  stdout: 'piped',
+});
 const process = command.spawn();
 const writer = process.stdin.getWriter();
 writer.write(new TextEncoder().encode(dotenv.stringify(env)));
@@ -50,9 +55,13 @@ Object.entries(env).forEach(([key, value]) => {
 });
 
 // output the environment variables
-if (Deno.args.includes("--export")) {
+if (Deno.args.includes('--export')) {
   Object.entries(env).forEach(([key, value]) => {
     console.log(`export ${key}="${value}"`);
+  });
+} else if (Deno.args.includes('--launchctl')) {
+  Object.entries(env).forEach(async ([key, value]) => {
+    await $.raw`launchctl setenv "${key}" "${value}"`;
   });
 } else {
   console.log(dotenv.stringify(env));
