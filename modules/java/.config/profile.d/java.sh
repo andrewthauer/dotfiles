eff273f981b8dfad8b6595a26ab47f1ce13af9a9
Andrew Thauer <6507159+andrewthauer@users.noreply.github.com>
2024-01-30 22:17:03 -0500

    chore: remove java module

--- modules/java/.config/profile.d/java.sh
+++
@@ -1,9 +0,0 @@
-#
-# Basic Java environment
-#
-
-# Set the JAVA_HOME
-if [[ -d "${XDG_DATA_HOME}/sdk/java/current" ]]; then
-  export JAVA_HOME="${XDG_DATA_HOME}/sdk/java/current"
-  export JDK_HOME="${JDK_HOME:-$JAVA_HOME}"
-fi
