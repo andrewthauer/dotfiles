6ed8097555eb4d451a83bfd712188b23ddedb43a
Andrew Thauer <6507159+andrewthauer@users.noreply.github.com>
2024-01-31 22:01:37 -0500

    chore: cleanup homebrew init

--- modules/homebrew/.config/profile.d/homebrew.sh
+++ modules/homebrew/.config/profile.d/homebrew.sh
@@ -2,19 +2,21 @@
 # Setup homebrew environment
 #
 
-if [[ "$OSTYPE" == darwin* ]]; then
-  if [ -d "/opt/homebrew" ]; then
-    eval "$(/opt/homebrew/bin/brew shellenv)"
+if [[ -z "${HOMEBREW_PREFIX}" ]]; then
+  if [[ "$OSTYPE" == darwin* ]]; then
+    if [ -d "/opt/homebrew" ]; then
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+    else
+      eval "$(/usr/local/bin/brew shellenv)"
+    fi
   else
-    eval "$(/usr/local/bin/brew shellenv)"
-  fi
-else
-  if [[ -x "$(command -v brew)" ]]; then
-    eval "$("$(brew --prefix)"/bin/brew shellenv)"
-  elif [[ -d "/home/linuxbrew/.linuxbrew" ]]; then
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-  elif [[ -d "$HOME/.linuxbrew" ]]; then
-    eval "$("$HOME"/.linuxbrew/bin/brew shellenv)"
+    if [[ -x "$(command -v brew)" ]]; then
+      eval "$("$(brew --prefix)"/bin/brew shellenv)"
+    elif [[ -d "/home/linuxbrew/.linuxbrew" ]]; then
+      eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    elif [[ -d "$HOME/.linuxbrew" ]]; then
+      eval "$("$HOME"/.linuxbrew/bin/brew shellenv)"
+    fi
   fi
 fi
 
