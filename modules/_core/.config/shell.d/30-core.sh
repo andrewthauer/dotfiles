840878b6a8853a832316e5c6dfccbcd9248eb82f
Andrew Thauer <6507159+andrewthauer@users.noreply.github.com>
2024-04-21 18:16:46 -0400

    core aliases

--- modules/_core/.config/shell.d/30-core.sh
+++ modules/_core/.config/shell.d/30-core.sh
@@ -6,3 +6,17 @@
 # Append bin directories to path
 prepend_path "${DOTFILES_DIR}/bin"
 prepend_path "${HOME}/.local/bin"
+
+# Go to the dotfiles directory
+if [ -n "${DOTFILES_DIR}" ]; then
+  alias dotdir='cd ${DOTFILES_DIR}'
+fi
+
+# Code directory
+export CODE_DIR="${CODE_DIR:-$HOME/src}"
+if [ -d "${CODE_DIR}" ]; then
+  alias codedir='cd ${CODE_DIR}'
+else
+  unset CODE_DIR
+fi
+
