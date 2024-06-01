c87da662a8d9c4140d5f273db866b9de5a56aa8d
Andrew Thauer <6507159+andrewthauer@users.noreply.github.com>
2024-06-01 09:52:48 -0400

    1password ssh agent setup

--- modules/1password/install.sh
+++ modules/1password/install.sh
@@ -1,1 +1,39 @@
 #!/usr/bin/env bash
+
+set -e
+
+configure_launch_agent() {
+  mkdir -p ~/Library/LaunchAgents
+
+  cat <<EOF >~/Library/LaunchAgents/com.1password.SSH_AUTH_SOCK.plist
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+  <key>Label</key>
+  <string>com.1password.SSH_AUTH_SOCK</string>
+  <key>ProgramArguments</key>
+  <array>
+    <string>/bin/sh</string>
+    <string>-c</string>
+    <string>/bin/ln -sf $HOME/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock \$SSH_AUTH_SOCK</string>
+  </array>
+  <key>RunAtLoad</key>
+  <true/>
+</dict>
+</plist>
+EOF
+
+  launchctl load -w ~/Library/LaunchAgents/com.1password.SSH_AUTH_SOCK.plist
+}
+
+main() {
+  case "$("$DOTFILES_BIN"/os-info --family)" in
+    "macos")
+      configure_launch_agent
+      ;;
+    *) ;;
+  esac
+}
+
+main "$@"
