d47efdc4032a60545df803557dd6edd6c92c6d10
Andrew Thauer <6507159+andrewthauer@users.noreply.github.com>
2024-12-30 08:37:44 -0500

    fix rust env setup

--- modules/rust/.config/shell.d/rust.sh
+++ modules/rust/.config/shell.d/rust.sh
@@ -4,11 +4,3 @@
 # - https://www.rust-lang.org
 #
 
-# Load rust manually
-if [ -d "${CARGO_HOME}/bin" ]; then
-  prepend_path "${CARGO_HOME}/bin"
-
-# Return if requirements not found
-else
-  return 1
-fi
