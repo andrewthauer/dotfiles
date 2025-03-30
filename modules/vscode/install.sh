41727d34efbfc5f022aa49fe85098ca582b35366
Andrew Thauer <6507159+andrewthauer@users.noreply.github.com>
2025-03-30 08:22:38 -0400

    update vscode

--- modules/vscode/install.sh
+++ modules/vscode/install.sh
@@ -5,12 +5,13 @@ set -eou pipefail
 DOTFILES_HOME="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." &>/dev/null && pwd)"
 PATH="$DOTFILES_HOME/bin:$PATH"
 
+LIBRARY_DIR="$HOME/Library/Application Support"
+SOURCE_DIR="$XDG_CONFIG_HOME/vscode"
+
 main() {
   case "$(os-info --family)" in
     macos)
       dotfiles module link "vscode"
-      SOURCE_DIR="$XDG_CONFIG_HOME/vscode"
-      LIBRARY_DIR="$HOME/Library/Application Support"
       # vscode
       ln -sf "$SOURCE_DIR/keybindings.json" "$LIBRARY_DIR/Code/User/keybindings.json"
       ln -sf "$SOURCE_DIR/settings.json" "$LIBRARY_DIR/Code/User/settings.json"
