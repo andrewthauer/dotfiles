d8b32ef1cc7b98083d200faeb07969df3d5fd4d4
Andrew Thauer <6507159+andrewthauer@users.noreply.github.com>
2025-05-12 08:54:04 -0400

    fix nvim terminal toggle

--- modules/neovim/.config/nvim/lua/plugins/ui.lua
+++ modules/neovim/.config/nvim/lua/plugins/ui.lua
@@ -161,7 +161,7 @@ return {
       { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse", mode = { "n", "v" } },
       { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
       { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
-      { "<c-/>",      function() Snacks.terminal() end, desc = "Toggle Terminal" },
+      { "<c-/>",      function() Snacks.terminal.toggle() end, desc = "Toggle Terminal", mode = { "n", "t" } },
       { "<c-_>",      function() Snacks.terminal() end, desc = "which_key_ignore" },
       { "]]",         function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
       { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
