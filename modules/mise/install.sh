5aac5753dc6be9641f17c73959d98398daac08a7
Andrew Thauer <6507159+andrewthauer@users.noreply.github.com>
2025-08-17 11:52:20 -0400

    misc changes

--- modules/mise/install.sh
+++ modules/mise/install.sh
@@ -5,20 +5,6 @@ set -eou pipefail
 DOTFILES_HOME="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." &>/dev/null && pwd)"
 PATH="$DOTFILES_HOME/bin:$PATH"
 
-mise_devcontainer_setup() {
-  # Move any existing mise config file to a backup
-  if [ -f "$XDG_CONFIG_HOME/mise/config.toml" ] && [ ! -L "$XDG_CONFIG_HOME/mise/config.toml" ]; then
-    mv "$XDG_CONFIG_HOME/mise/config.toml" "$XDG_CONFIG_HOME/config.toml.old"
-  fi
-
-  # Custom mise config to avoid conflicts with mounted volumes or base images
-  if [ -n "$MISE_GLOBAL_CONFIG_FILE" ]; then
-    cat >"$DOTFILES_LOCAL_MODULE_PATH/.config/environment.d/mise.conf" <<EOF
-MISE_GLOBAL_CONFIG_FILE="${MISE_GLOBAL_CONFIG_FILE}"
-EOF
-  fi
-}
-
 main() {
   if [ "$(command -v mise)" ]; then
     echo "mise is already installed"
@@ -27,11 +13,6 @@ main() {
       "macos") pkg install --type brew mise ;;
       *) curl https://mise.jdx.dev/install.sh | sh ;;
     esac
-  fi
-
-  # Special handling for devcontainer setup
-  if [ -n "${REMOTE_CONTAINERS:-}" ] || [ -n "${DEVPOD:-}" ]; then
-    mise_devcontainer_setup
   fi
 
   # Ensure the dotfiles module is available
