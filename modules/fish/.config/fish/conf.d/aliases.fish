e3cf667c9440ff0f60760a2caf4d51843ec8e360
Andrew Thauer <6507159+andrewthauer@users.noreply.github.com>
2025-11-30 18:04:56 -0500

    fish updates

--- modules/fish/.config/fish/conf.d/aliases.fish
+++ modules/fish/.config/fish/conf.d/aliases.fish
@@ -1,8 +1,3 @@
 # Directory listing
 alias l="ls -a"
 alias ll="ls -la"
-
-# Source all module aliases
-for script in "$XDG_CONFIG_HOME/alias.d"/*.{sh,fish}
-  source $script
-end
--- modules/fish/.config/fish/config.fish
+++ modules/fish/.config/fish/config.fish
@@ -1,34 +1,37 @@
 # Check for custom dotfiles home
 if test -f "$XDG_CONFIG_HOME/dotfiles-home"
-  set -gx DOTFILES_HOME $(cat "$XDG_CONFIG_HOME/dotfiles-home")
+    set -gx DOTFILES_HOME $(cat "$XDG_CONFIG_HOME/dotfiles-home")
 end
 
 # Setup default dotfiles env vars
 if not set -q DOTFILES_HOME
-  set -gx DOTFILES_HOME $HOME/.dotfiles
+    set -gx DOTFILES_HOME $HOME/.dotfiles
 end
 
-# TODO: load environment
-# https://gist.github.com/nikoheikkila/dd4357a178c8679411566ba2ca280fcc
-# function envsource
-#   for line in (cat $argv | grep -v '^#')
-#     set item (string split -m 1 '=' $line)
-#     set -gx $item[1] $item[2]
-#     echo "Exported key $item[1]"
-#   end
-# end
-# echo "loading env"
-# for env_file in "$XDG_CONFIG_HOME/environment.d"/*.conf
-#   # echo "$env_file"
-#   for line in (cat $env_file | grep -v -E '^$|^#')
-#     set item (string split -m 1 '=' $line)
-#     echo $item
-#     # set -gx "$item[1]" "$item[2]"
-#     # echo "Exported key $item[1]"
-#   end
-# end
+# Export all variables from nvironment.d
+if test -d "$XDG_CONFIG_HOME/environment.d"
+    for file in $XDG_CONFIG_HOME/environment.d/*.conf
+        if test -r "$file"
+            cat "$file" | while read -l line
+                if test -n "$line"; and not string match -qr '^#' -- "$line"
+                    set -l parts (string split -m 1 '=' -- "$line")
+                    if test (count $parts) -eq 2
+                        set -gx $parts[1] $parts[2]
+                    end
+                end
+            end
+        end
+    end
+end
+
+# Source all module aliases
+if test -d "$XDG_CONFIG_HOME/alias.d"
+    for script in "$XDG_CONFIG_HOME/alias.d"/*.{sh,fish}
+        source $script
+    end
+end
 
 if status is-interactive
-  # Commands to run in interactive sessions can go here
-  set -U fish_greeting
+    # Commands to run in interactive sessions can go here
+    set -U fish_greeting
 end
